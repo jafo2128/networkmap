2663b140bf00d021eb0f2c3cdb3ba16b5c955ef6
shinjw <jonathanwshin@gmail.com>
2014-10-10 07:35:15 +0900

    DB Util Classes

--- src/main/java/com/pk/network/map/installer/DBUtil.java
+++ src/main/java/com/pk/network/map/installer/DBUtil.java
@@ -1,8 +1,15 @@
 package com.pk.network.map.installer;
 
+import java.sql.SQLException;
+
 import org.h2.tools.Server;
 public class DBUtil {
-	Server server = Server.createTcpServer("networkmap").start();
+	
+	// First Run - Installation Utility - Create Server Connection()
+	public void createServer() throws SQLException {
+		Server server = Server.createTcpServer("networkmap").start();
+	}
+	
 	
 
 }
